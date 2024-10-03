84bcbae23e9eb0e381a597f3f7bf73e5b2184f54
Wenhui Li <wenhuili@Wenhuis-MacBook-Air.local>
2024-10-03 21:37:41 +0800

    fix strings4

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -18,11 +18,11 @@ fn main() {
     string_slice("blue");
     string("red".to_string());
     string(String::from("hi"));
-    string_slice("rust is fun!".to_owned());
+    string("rust is fun!".to_owned());
     string_slice("nice weather".into());
-    string_slice(format!("Interpolation {}", "Station"));
+    string(format!("Interpolation {}", "Station"));
     string_slice(&String::from("abc")[0..1]);
     string_slice("  hello there ".trim());
-    string_slice("Happy Monday!".to_string().replace("Mon", "Tues"));
-    string_slice("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
