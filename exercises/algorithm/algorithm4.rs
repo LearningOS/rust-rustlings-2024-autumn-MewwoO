f0ee2fa9cde30921341892679802d360a0e64f06
MewwoO <136430035+MewwoO@users.noreply.github.com>
2024-10-18 03:39:55 +0000

    finish algorithm4

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -51,12 +50,39 @@ where
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
         //TODO
+        let mut current = &mut self.root;
+
+        while let Some(ref mut node) = current {
+            if value < node.value {
+                current = &mut node.left;
+            } else if value > node.value {
+                current = &mut node.right;
+            } else {
+                // The value already exists, do nothing
+                return;
+            }
+        }
+
+        // Insert the new value where current is `None`
+        *current = Some(Box::new(TreeNode::new(value)));
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
         //TODO
-        true
+        // true
+        let mut current = &self.root;
+
+        while let Some(ref node) = current {
+            match value.cmp(&node.value) {
+                Ordering::Less => current = &node.left,
+                Ordering::Greater => current = &node.right,
+                Ordering::Equal => return true,  // Value found
+            }
+        }
+
+        // Value not found in the tree
+        false
     }
 }
 
